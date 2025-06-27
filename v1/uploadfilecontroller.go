46ed8ab2d249894b1c0c35ef873744b368ae06fe
Callum <callumery11@outlook.com>
2025-06-27 21:54:24 +0100

    handle error

--- v1/uploadfilecontroller.go
+++ v1/uploadfilecontroller.go
@@ -14,6 +14,11 @@ func uploadDocument(c *gin.Context) {
 	body := &Base64DocumentString{}
 	if err := c.ShouldBindJSON(&body); err != nil {
 		fmt.Printf("Document received!\n")
+
+		err := database.WithConnection(uploadDocumentSQL)
+		if err != nil {
+			c.JSON(200, gin.H{"Error": err})
+		}
 	} else {
 		c.JSON(http.StatusInternalServerError, err)
 	}
