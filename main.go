1d055358478edc4c0844db7789adb679ca7d901d
Callum <callumery11@outlook.com>
2025-07-08 21:20:51 +0100

    Created integration tests

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"pdf_service_api/database"
 	v1 "pdf_service_api/v1"
+	"pdf_service_api/v1/controller"
+	"pdf_service_api/v1/repositories"
 )
 
 func main() {
-	documentController := controller.NewDocumentController(repositories.NewDocumentRepository())
-	router := v1.SetupRouter(*documentController)
+	dbConfig := database.ConfigForDatabase{}
+	documentController := controller.NewDocumentController(repositories.NewDocumentRepository(dbConfig))
+	router := v1.SetupRouter(documentController)
 	_ = router.Run() // listen and serve on 0.0.0.0:8080
 }
