ee7c00ff6a2a9ad2e3091ab73754df7bf52939f7
Callum <callumery11@outlook.com>
2025-07-14 13:31:24 -0700

    rename

--- controller/v1/test/integration/selection_controller_integration_test.go
+++ controller/v1/test/integration/selection_controller_integration_test.go
@@ -60,7 +60,7 @@ func deleteSelections(t *testing.T) {
 // /api/v1/documents/:id/selections/
 func deleteSelectionsUuidDoesNotExist(t *testing.T) {
 	t.Parallel()
-	router := testutil.CreateV1Router(t, dbUser, dbPassword)
+	router := testutil.CreateV1RouterAndPostgresContainer(t, dbUser, dbPassword)
 
 	w := httptest.NewRecorder()
 	router.ServeHTTP(w, httptest.NewRequest(
