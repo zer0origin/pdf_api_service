1c362aad0ea611e248ba5702d6d00848a78a049e
Callum <callumery11@outlook.com>
2025-07-19 03:43:25 -0700

    comment out unfinished test

--- controller/v1/test/integration/meta_controller_integration_test.go
+++ controller/v1/test/integration/meta_controller_integration_test.go
@@ -1,17 +1,11 @@
 package integration
 
-import (
-	"pdf_service_api/testutil"
-	"testing"
-)
-
-func TestMetaIntegration(t *testing.T) {
-	t.Run("getMetaDataFromDatabase", getMetaDataFromDatabase)
-}
-
-func getMetaDataFromDatabase(t *testing.T) {
-	t.Parallel()
-	router := testutil.CreateV1RouterAndPostgresContainer(t, "BasicSetupWithOneDocumentTableEntryTwoSelectionsAndMetaData", dbUser, dbPassword)
-
-	GetMetaR
-}
+//
+//func TestMetaIntegration(t *testing.T) {
+//	t.Run("getMetaDataFromDatabase", getMetaDataFromDatabase)
+//}
+//
+//func getMetaDataFromDatabase(t *testing.T) {
+//	t.Parallel()
+//	router := testutil.CreateV1RouterAndPostgresContainer(t, "BasicSetupWithOneDocumentTableEntryTwoSelectionsAndMetaData", dbUser, dbPassword)
+//}
