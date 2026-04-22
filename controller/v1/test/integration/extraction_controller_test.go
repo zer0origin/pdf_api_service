95eaba1efb8387144b065615540dace617cbd8f8
Callum <callumery11@outlook.com>
2026-04-22 04:46:52 +0100

    updated skip flags

--- controller/v1/test/integration/extraction_controller_test.go
+++ controller/v1/test/integration/extraction_controller_test.go
@@ -25,6 +25,10 @@ func TestExtractionIntegration(t *testing.T) {
 }
 
 func getTextFromSelectionUuidBase64NotIncluded(t *testing.T) {
+	if *testutil.SkipExtractionTest {
+		t.Skip("Skipping test due to flags")
+	}
+
 	t.Parallel()
 	selectionUids := []uuid.UUID{uuid.MustParse("a5fdea38-0a86-4c19-ae4f-c87a01bc860d"), uuid.MustParse("335a6b95-6707-4e2b-9c37-c76d017f6f97")}
 
